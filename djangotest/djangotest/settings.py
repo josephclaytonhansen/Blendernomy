d8eec786b53d386b41bcd8ddc9c209f3c8548b15
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2022-03-26 08:35:47 -0500

    settings

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -15,6 +15,8 @@ import mimetypes
 mimetypes.add_type("text/css", ".css", True)
 
 import environ, os
+import sys
+import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()
@@ -104,12 +106,19 @@ WSGI_APPLICATION = 'djangotest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
 
 
 # Password validation
@@ -154,6 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
