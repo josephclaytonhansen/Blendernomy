713933592e9bc723fb6009745f71e02b14e58dbf
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2022-03-26 10:47:14 -0500

    Update settings.py

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -171,8 +171,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
