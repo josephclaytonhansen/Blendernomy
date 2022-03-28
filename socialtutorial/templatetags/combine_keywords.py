cb00d481aac65a5e6ae74f06b3412ed875384fec
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2022-03-28 08:47:56 -0500

    meta keywords

--- socialtutorial/templatetags/combine_keywords.py
+++ socialtutorial/templatetags/combine_keywords.py
@@ -3,5 +3,7 @@ register = template.Library()
 
 @register.simple_tag
 def get_keywords(val=None):
+    article= val
+    keywords = val.meta_keywords
     s = "blender,blender3d,blender tutorials,blender resources,blender3d tutorials,stylized art,blender texture painting, blender nodes, blender shaders, shader nodes, blender npr, blendernomy,"
-    return s+val
+    return s+keywords
